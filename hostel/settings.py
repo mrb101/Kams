c3f3a4c862d3e7b19bc39f51539db703dfcaba35
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-05-11 07:59:06 +0000

    modifies static root

--- hostel/settings.py
+++ hostel/settings.py
@@ -133,5 +133,5 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     )
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media_cdn')
