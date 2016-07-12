89a664ad08642e5ff71fc8214dfebc41e980ec51
Bassel J. Hamadeh <hamadeh.basel@gmail.com>
2016-07-12 14:57:50 +0800

    fixes bucket var name

--- hostel/settings.py
+++ hostel/settings.py
@@ -144,7 +144,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-S3_URL = 'https://%s.s3.amazonaws.com' % S3_STORAGE_BUCKET_NAME
+S3_URL = 'https://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
